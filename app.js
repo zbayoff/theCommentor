6757d6f2cff38a661868c8867b4033c57c59f2fd
Zach Bayoff <zachbayoff@Zachs-MacBook-Pro.local>
2018-04-11 07:58:49 -0400

    outputting db results to comments page

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use('/public', express.static('public'));
 // Global vars
 app.use(function (req, res, next) {
   res.locals.errors = null;
+  res.locals.success = null;
   next();
 });
 
@@ -33,28 +34,109 @@ MongoClient.connect('mongodb://zrbayoff:theroad12345@ds239359.mlab.com:39359/the
   });
 });
 
+app.get('/comments', (req, res) => {
+  // res.sendFile(__dirname + '/index.html');
+  db
+    .collection('comments')
+    .find()
+    .toArray((err, result) => {
+      if (err) {
+        return console.log(err)
+      } else {
+
+        let name = result.map(result => result.name);
+
+        let message = result.map(result => result.message);
+        let dateAdded = result.map(result => result.dateAdded);
+
+        const comments = [{
+          name: name
+        }, {
+          message: message
+        }, {
+          dateAdded: dateAdded
+        }];
+
+        res.render('comments', {
+          comments: {
+            name: name,
+            message: message,
+            dateAdded: dateAdded
+          }
+        });
+      }
+    });
+});
+
 // Post request
 app.post('/comments', [
   // check name and message fields for values
-  check('name', 'Please enter your name.').isLength({min:1}),
-  check('message', 'Please write a message.').isLength({min:1})
+  check('name', 'Please enter your name.').isLength({
+    min: 1
+  }),
+  check('message', 'Please write a message.').isLength({
+    min: 1
+  })
 
 ], (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    res.redirect('/');
-    const errorObj = errors.mapped();
-    console.log(errorObj);
-    
+    res.render('index', {
+      errors: errors.array()
+    })
+
   } else {
-    db.collection('comments').save(req.body, (err, result) => {
+
+    let time = new Date()
+    let newComment = req.body;
+    newComment['dateAdded'] = time.toString();
+
+    db.collection('comments').save(newComment, (err, result) => {
       if (err) {
         return console.log(err);
       } else {
-        console.log('saved to database');
-        res.redirect('/');
+        // res.render('comments', {
+        //   success: 'Saved to Database!'
+        // });
       }
     });
+
+    db
+    .collection('comments')
+    .find()
+    .toArray((err, result) => {
+      if (err) {
+        return console.log(err)
+      } else {
+
+        let name = result.map(result => result.name);
+
+        let message = result.map(result => result.message);
+        let dateAdded = result.map(result => result.dateAdded);
+
+        const comments = [{
+          name: name
+        }, {
+          message: message
+        }, {
+          dateAdded: dateAdded
+        }];
+
+        res.render('comments', {
+          comments: {
+            name: name,
+            message: message,
+            dateAdded: dateAdded
+          }
+        });
+      }
+    });
+
+    
+
+
+
+
   }
 });
 
@@ -64,6 +146,8 @@ app.get('/', (req, res) => {
   // res.sendFile(__dirname + '/index.html');
   res.render('index');
 });
+
+
 
 // app.listen(port, () => {
 //     console.log(`Listening on port ${port}`);
