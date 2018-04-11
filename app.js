cfed16f8774e7d087a4e011a3fb5da3f3367438f
zach bayoff <zrbayoff@gmail.com>
2018-04-11 16:56:13 -0400

    comments page working, now stlying dates, adding images

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const moment = require('moment');
 const MongoClient = require('mongodb').MongoClient;
 const {
   check,
@@ -11,6 +12,8 @@ const {
 } = require('express-validator/filter');
 const app = express();
 const port = 9000;
+
+moment().format();
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -25,7 +28,12 @@ app.use(function (req, res, next) {
   next();
 });
 
-// to connect to database
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
 MongoClient.connect('mongodb://zrbayoff:theroad12345@ds239359.mlab.com:39359/thecommentor', (err, database) => {
   if (err) return console.log(err);
   db = database;
@@ -35,17 +43,18 @@ MongoClient.connect('mongodb://zrbayoff:theroad12345@ds239359.mlab.com:39359/the
 });
 
 app.get('/comments', (req, res) => {
-  // res.sendFile(__dirname + '/index.html');
   db
     .collection('comments')
     .find()
+    .sort({
+      dateAdded: -1
+    })
     .toArray((err, result) => {
       if (err) {
         return console.log(err)
       } else {
 
         let name = result.map(result => result.name);
-
         let message = result.map(result => result.message);
         let dateAdded = result.map(result => result.dateAdded);
 
@@ -68,87 +77,30 @@ app.get('/comments', (req, res) => {
     });
 });
 
-// Post request
-app.post('/comments', [
-  // check name and message fields for values
+app.post('/', [
   check('name', 'Please enter your name.').isLength({
     min: 1
   }),
   check('message', 'Please write a message.').isLength({
     min: 1
   })
-
 ], (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     res.render('index', {
       errors: errors.array()
     })
-
   } else {
-
     let time = new Date()
     let newComment = req.body;
-    newComment['dateAdded'] = time.toString();
+    newComment['dateAdded'] = time;
 
     db.collection('comments').save(newComment, (err, result) => {
       if (err) {
         return console.log(err);
       } else {
-        // res.render('comments', {
-        //   success: 'Saved to Database!'
-        // });
+        res.redirect('/comments');
       }
     });
-
-    db
-    .collection('comments')
-    .find()
-    .toArray((err, result) => {
-      if (err) {
-        return console.log(err)
-      } else {
-
-        let name = result.map(result => result.name);
-
-        let message = result.map(result => result.message);
-        let dateAdded = result.map(result => result.dateAdded);
-
-        const comments = [{
-          name: name
-        }, {
-          message: message
-        }, {
-          dateAdded: dateAdded
-        }];
-
-        res.render('comments', {
-          comments: {
-            name: name,
-            message: message,
-            dateAdded: dateAdded
-          }
-        });
-      }
-    });
-
-    
-
-
-
-
   }
 });
-
-app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-  // res.sendFile(__dirname + '/index.html');
-  res.render('index');
-});
-
-
-
-// app.listen(port, () => {
-//     console.log(`Listening on port ${port}`);
-// });
